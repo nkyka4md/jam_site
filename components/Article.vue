8445f4b0e59d2b57243c091ad8f2e5489ecdc42d
Kaito Nakaya <knakaya@valuecommerce.co.jp>
2021-09-29 10:20:51 +0900

    記事の個別ページを作成

--- components/Article.vue
+++ components/Article.vue
@@ -4,14 +4,15 @@
       class="mb-3 w-full h-64 bg-center bg-cover"
       :style=" 'background-image: url(' + work.fields.image.fields.file.url + ')' "
     ></div>
-    <h3 class="ml-3 font-bold">{{ work.fields.title }}</h3>
+    <nuxt-link :to=" '/work/' + work.fields.slug ">
+      <h3 class="ml-3 font-bold">{{ work.fields.title }}</h3>
+    </nuxt-link>
     <h4 class="ml-3 my-2 text-xs">{{ work.fields.subtitle }}</h4>
     <div class="flex ml-2">
       <li
         v-for="tag in work.fields.tag"
         :key="tag.sys.id"
-        class="list-none text-xs m-1 bg-gray-200 p-1 rounded cursor-pointer"
-      >
+        class="list-none text-xs m-1 bg-gray-200 p-1 rounded cursor-pointer">
         {{ tag.fields.name }}
       </li>
     </div>
