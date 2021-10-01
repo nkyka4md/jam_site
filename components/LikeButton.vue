86fa59862ef309482328dd9872726907171fb5e1
Kaito Nakaya <knakaya@valuecommerce.co.jp>
2021-10-01 17:29:11 +0900

    いいねボタン作成

---
+++ components/LikeButton.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+  <div id="likeButton">
+    <like-component></like-component>
+    <like-component></like-component>
+    <like-component></like-component>
+  </div>
+  </div>
+</template>
+
+<script>
+  (function() {
+    'use strict';
+
+    var likeComponent = Vue.extend({
+      data: function() {
+        return {
+          count: 0
+        }
+      },
+      template: '<button @click="countUp">Like {{ count }}</button>',
+      methods: {
+        countUp: function() {
+          this.count++;
+        }
+      }
+    });
+
+    var app = new Vue({
+      el: '#app',
+      components: {
+        'like-component': likeComponent
+      }
+    });
+
+  })();
+</script>
+
+
