8efcbc3509182033d75c4f2155b32118b4380d70
Kaito Nakaya <knakaya@valuecommerce.co.jp>
2021-10-28 13:52:22 +0900

    email入力フォームの枠の色調整

--- pages/profile.vue
+++ pages/profile.vue
@@ -28,7 +28,7 @@
               <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
                 メールアドレス
               </label>
-              <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="example@gmail.com">
+              <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="example@gmail.com">
             </div>
             <div class="mb-6">
               <label class="block text-gray-700 text-sm font-bold mb-2" for="message">
@@ -37,7 +37,7 @@
               </label>
             </div>
             <div class="flex items-center justify-between">
-              <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+              <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
                 送信
               </button>
             </div>
